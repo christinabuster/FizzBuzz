b769648c9c5499f63d600ef1f5c99fe085b8488f
christinabuster <christina.buster13@gmail.com>
2017-01-24 14:59:25 -0800

    wip

--- fizzbuzz_spec.rb
+++ fizzbuzz_spec.rb
@@ -3,19 +3,35 @@ require_relative 'fizzbuzz'
 
 describe "fizzbuzz" do
   context 'if a number is divisible by' do
-
-    it "print 'fizz' when a number is divisible by 3" do
-
+    it '3' do
+      expect(is_divisible_by_three?(3)).to be_truthy
     end
-
-    it "print 'buzz' when a number is divisible by 5" do
-
+    it '5' do
+      expect(is_divisible_by_five?(5)).to be_truthy
     end
+    it '15' do
+      expect(is_divisible_by_fifteen?(15)).to be_truthy
+    end
+  end
 
-    it "print 'fizzbuzz' when a number is divisible by 3 and 5" do
+  context 'if a number is not divisible by' do
+    it '3' do
+      expect(is_divisible_by_three?(1)).to be_falsey
+    end
+    it '5' do
+      expect(is_divisible_by_five?(1)).to be_falsey
+    end
+    it '15' do
+      expect(is_divisible_by_fifteen?(1)).to be_falsey
+    end
+  end
 
+  context 'returns fizzbuzz'
+    it 'the number' do
+      expect(fizzbuzz(1)).to eq 1
+    end
+    it 'fizz' do
+      expect(fizzbuzz(3)).to eq "Fizz"
     end
   end
 end
-
-I have no input only output
