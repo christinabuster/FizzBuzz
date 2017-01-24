02655be9f3060c037caf56ccfcbff12cbc0d70e5
christinabuster <christina.buster13@gmail.com>
2017-01-24 15:03:18 -0800

    add first test

--- fizzbuzz_spec.rb
+++ fizzbuzz_spec.rb
@@ -1,19 +1,10 @@
+require 'rspec'
+require_relative 'fizzbuzz'
+
 describe "fizzbuzz" do
   context 'if a number is divisible by' do
-
-    it "print "fizz" when a number is divisible by 3" do
-      # tests that the number instead of the number 3 Fizz is printed not if divisible
-      fizz_3 = fizzbuzz(3)
-      expect(fizz_3).to eq("Fizz")
-    end
-
-    it "print "buzz" when a number is divisible by 5" do
-      fizz_3 = fizzbuzz(5)
-      expect(fizz_5).to eq("Buzz")
-    end
-
-    it 'print "fizzbuzz" when a number is divisible by 3 and 5' do
-      expect(fizzbuzz(x)).to eq("FizzBuzz")
+    it '3' do
+      expect(is_divisible_by_three?(3)).to be_truthy
     end
   end
 end
