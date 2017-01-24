e819377f7c8ce15dd92a4d74efc7d2fa00ada8db
christinabuster <christina.buster13@gmail.com>
2017-01-24 15:11:25 -0800

    add divisible by 5 method

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,7 @@
-def is_divisible_by_three?(number)
-  number % 3 == 0
+def fizz(i)
+  i % 3 == 0
+end
+
+def buzz(i)
+  i % 5 == 0
 end
--- fizzbuzz_spec.rb
+++ fizzbuzz_spec.rb
@@ -4,7 +4,10 @@ require_relative 'fizzbuzz'
 describe "fizzbuzz" do
   context 'if a number is divisible by' do
     it '3' do
-      expect(is_divisible_by_three?(3)).to be_truthy
+      expect(fizz(3)).to be_truthy
+    end
+    it '5' do
+      expect(buzz(5)).to be_truthy
     end
   end
 end
