3a655d6f0abb247cb01224713471c56692a25004
christinabuster <christina.buster13@gmail.com>
2017-01-24 15:15:44 -0800

    added test for non divisibility

--- fizzbuzz_spec.rb
+++ fizzbuzz_spec.rb
@@ -13,4 +13,16 @@ describe "fizzbuzz" do
       expect(fizzbuzz(15)).to be_truthy
     end
   end
+
+  context 'if a number is not divisible by' do
+    it '3' do
+      expect(fizz(1)).to be_falsey
+    end
+    it '5' do
+      expect(buzz(1)).to be_falsey
+    end
+    it '15' do
+      expect(fizzbuzz(1)).to be_falsey
+    end
+  end
 end
