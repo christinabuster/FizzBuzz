9ec17803882303cf73dcaf52dd2b20119752cb93
christinabuster <christina.buster13@gmail.com>
2017-01-24 16:29:15 -0800

    fix rspec path

--- fizzbuzz_spec.rb
+++ fizzbuzz_spec.rb
@@ -1,5 +1,5 @@
 require 'rspec'
-require_relative 'fizzbuzz'
+require_relative 'fizz_buzz'
 
 describe "fizzbuzz" do
   context 'if a number is divisible by' do
@@ -27,16 +27,16 @@ describe "fizzbuzz" do
   end
 
   context 'when implimenting fizzbuzz' do
-    it 'prints numbers' do
+    it 'should print numbers that are not divisible by 3 or 5' do
       expect(fizz_buzz(1)).to eq 1
     end
-    it 'prints fizz for the number 3' do
+    it 'should print fizz for the number 3' do
       expect(fizz_buzz(3)).to eq "Fizz"
     end
-    it 'prints buzz for the number 3' do
+    it 'should print buzz for the number 5' do
       expect(fizz_buzz(5)).to eq "Buzz"
     end
-    it 'prints FizzBuzz for the number 3' do
+    it 'should print FizzBuzz for the number 15' do
       expect(fizz_buzz(15)).to eq "FizzBuzz"
     end
   end
